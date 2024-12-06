a8f9c51f0a9fbe2c2339e095789cdfed8d70f10d
Mukho <mukho@kakao.com>
2024-12-06 14:33:49 +0900

    Init: 프로젝트 기본 구조 생성

---
+++ src/main/java/com/mukho/mukhogg/mukhogg/MukhoggApplication.java
@@ -0,0 +1,13 @@
+package com.mukho.mukhogg.mukhogg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukhoggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukhoggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mukho/mukhogg/mukhogg/MukhoggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukho.mukhogg.mukhogg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukhoggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
