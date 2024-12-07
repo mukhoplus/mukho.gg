e71f59ce1db8fd36f0db71af384af5a6f9960734
Mukho <mukho@kakao.com>
2024-12-07 23:29:14 +0900

    Fix: 오류 처리 수정

--- src/main/java/com/mukho/mukhogg/controller/MukhoggController.java
+++ src/main/java/com/mukho/mukhogg/controller/MukhoggController.java
@@ -49,9 +49,17 @@ public class MukhoggController {
             List<ParticipantDto> participants = matchInfo.getInfo().getParticipants();
             model.addAllAttributes(calculateTeamStats(participants));
         } catch (Exception e) {
+            String errMsg = e.getMessage();
+            System.out.println(e.getMessage());
+            if (errMsg.contains("\"matchInfo\" is null")) {
+                model.addAttribute("error", "최근 매치 기록이 없습니다.");
+            } else if (errMsg.contains("\"summonerInfo\" is null")) {
+                model.addAttribute("error", "소환사를 찾을 수 없습니다.");
+            } else {
+             model.addAttribute("error", "오류가 발생했습니다.");
+            }
+            
             initModels(model);
-            System.err.println(e.getMessage());
-            model.addAttribute("error", "소환사를 찾을 수 없습니다.");
         }
         
         return "main";
